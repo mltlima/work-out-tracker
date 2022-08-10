adad9da47e5cefe40aeec894d2d2e82a4c3fcf7d
mltlima <miguellt@dcc.ufrj.br>
2022-08-09 23:10:35 -0300

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,20 @@
 import { Box, Button, Divider, Link, TextField, Typography,Paper, BottomNavigation, BottomNavigationAction  } from "@mui/material";
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
+import FitnessCenterOutlinedIcon from '@mui/icons-material/FitnessCenterOutlined';
+import { useNavigate } from "react-router-dom";
 
 
 export default function Footer() {
-
+    const navigate = useNavigate();
 
     return(
         <Paper sx={footerStyle} elevation={3}>
             <BottomNavigation>
-                <BottomNavigationAction label="Home" icon={<HomeOutlinedIcon />} />
+                <BottomNavigationAction label="Home" icon={<HomeOutlinedIcon />} onClick={() => navigate("/")}/>
+                <BottomNavigationAction label="Progress" icon={<InsertChartOutlinedIcon />} />
+                <BottomNavigationAction label="Exercises" icon={<FitnessCenterOutlinedIcon />} />
                 <BottomNavigationAction label="Profile" icon={<PersonOutlineOutlinedIcon />} />
             </BottomNavigation>
         </Paper>
